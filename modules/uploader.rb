c9f9b6f9f131b3da3690d7f8f58fc39acf9d5b44
Froz10 <ivan.frozby@gmail.com>
2021-01-15 23:27:42 +0600

    Change method 'load_db'

--- modules/uploader.rb
+++ modules/uploader.rb
@@ -6,12 +6,13 @@ module Uploader
   end
 
   def load_db
-    check_exist_db_file
-    list = [Author, Book, Reader, Order, Date, Time]
-    YAML.safe_load(File.read(FILE_PATH), list, [], [], true).to_a.flatten
+    begin
+      list = [Author, Book, Reader, Order, Date, Time]
+      YAML.safe_load(File.read(FILE_PATH), list, [], [], true).to_a.flatten
+    rescue 
+      File.new(FILE_PATH, 'w')
+      retry
+    end
   end
 
-  def check_exist_db_file
-    File.write(FILE_PATH, '') unless File.exist?(FILE_PATH)
-  end
 end
