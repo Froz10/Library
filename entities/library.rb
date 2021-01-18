4bbea6621560acb81273ccb330cfbe8949541094
Froz10 <ivan.frozby@gmail.com>
2021-01-18 23:58:58 +0600

    Create methods: Top Reader, Most Popular Books, Number of Readers of the Most Popular Books

--- entities/library.rb
+++ entities/library.rb
@@ -25,4 +25,18 @@ class Library
     data = [@authors, @books, @readers, @orders]
     save_to_db(data)
   end
+
+  def top_readers(quantity = 1)
+    @orders.group_by(&:reader).sort_by { |_reader, order| -order.size }.to_h.keys.first(quantity)
+  end
+
+  def top_books(quantity = 1)
+    @orders.group_by(&:book).sort_by { |_book, order| -order.size }.to_h.keys.first(quantity)
+  end
+
+  def number_of_readers_of_top_books(quantity = 3)
+    @orders.group_by(&:book)
+           .sort_by { |_book, order| -order.size }
+           .first(quantity).to_h.values.flatten.uniq(&:reader).size
+  end
 end
