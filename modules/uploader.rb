3560950884409c578498fb0f77084d5299045719
Froz10 <ivan.frozby@gmail.com>
2022-03-22 11:29:58 +0600

    refactoring uploader

--- modules/uploader.rb
+++ modules/uploader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Uploader
+  DIR_PATH = './db/'
   FILE_PATH = './db/data.yml'
 
   def save_to_db(data)
@@ -8,10 +9,11 @@ module Uploader
   end
 
   def load_db
-    list = [Author, Book, Reader, Order, Statistics]
+    list = [Author, Book, Reader, Order, Statistics, Time]
     begin
-      YAML.safe_load(File.read(FILE_PATH), list).to_a.flatten
+      YAML.safe_load(File.read(FILE_PATH), list, [], [], true).to_a.flatten
     rescue StandardError
+      Dir.mkdir(DIR_PATH) unless File.exist?(DIR_PATH)
       File.new(FILE_PATH, 'w')
       retry
     end
