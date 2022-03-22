87640443bb57fb6d049fbc6ec29cd8006e3bc514
Froz10 <ivan.frozby@gmail.com>
2022-03-22 11:48:48 +0600

    fix issue

--- modules/uploader.rb
+++ modules/uploader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Uploader
+  DIR_PATH = './db/'
   FILE_PATH = './db/data.yml'
 
   def save_to_db(data)
@@ -12,6 +13,7 @@ module Uploader
     begin
       YAML.safe_load(File.read(FILE_PATH), list, [], [], true).to_a.flatten
     rescue StandardError
+      Dir.mkdir(DIR_PATH) unless File.exist?(DIR_PATH)
       File.new(FILE_PATH, 'w')
       retry
     end
